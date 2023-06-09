74005331e87f9bf16d3a53e913b45d94e9efaf7a
erikafallacara <93384851+erikafallacara@users.noreply.github.com>
2023-06-09 10:18:37 +0200

    Delete params.py

--- params.py
+++
@@ -1,8 +0,0 @@
-import os
-
-##################  VARIABLES  ##################
-NUM_OF_LABELS = os.environ.get('NUM_OF_LABELS')
-RAW_DATA_DIR = os.path.join('.', 'raw_data', 'Train')
-PREPROC_DATA_DIR = os.path.join('.', 'preproc_data')
-STORAGE_DATA_KEY = 'gcs'
-BUCKET_NAME = 'project_pagodas'
