9a81e5a78b2e811651e90ddfa5fc8a6178907282
Julien Tetar <julien.tetar@edhec.com>
2023-06-09 08:27:05 +0200

    lstm model function and amended save/load models

--- params.py
+++ params.py
@@ -4,3 +4,5 @@ import os
 NUM_OF_LABELS = os.environ.get('NUM_OF_LABELS')
 RAW_DATA_DIR = os.path.join('.', 'raw_data', 'Train')
 PREPROC_DATA_DIR = os.path.join('.', 'preproc_data')
+STORAGE_DATA_KEY = 'gcs'
+BUCKET_NAME = 'project_pagodas'
